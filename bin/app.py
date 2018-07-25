654bb0751902f0f3a4fad2fbd0594bc15531f207
kishor <kishorrathva8298@gmail.com>
2018-07-25 16:06:39 +0530

    App

--- bin/app.py
+++ bin/app.py
@@ -1,21 +1,47 @@
 import web
+from gamme import map
 
 urls = (
-	'/hello','Index'
+	'/game','GameEngine'
+	'/','Index'
 	)
 
 app = web.application(urls,globals())
+
+# little hack so that debug mode works with sessions
+if web.config.get('_session') is None:
+	store = web.session.DiskStore('sessions')
+	session =web.session.Session(app,store,initializer={'room:'None})
+
+	web.config._session = session
+else:
+	session = web.config._session
+
 render = web.template.render('templates/',base="layout")
 
 
 class Index(object):
 	def GET(self):
-		return  render.hello_form()
+		#This is used to  "setup the session with starting values"
+		session.room = map.START
+		web.seeother("/game")
+
+
+class GameEngine(object):
+
+	def GET(self):
+		if session.room:
+			return render.show_room(room=session.room)
+		else:
+			return render.you_died()
 
 	def POST(self):
-		form = web.input(name="Nobody",greet="Hello")
-		greeting = "%s ,%s" % (form.greet ,form.name)
-		return render.index(greeting = greeting)
+		form = web.input(action = None)
+
+		if session.room and form.action:
+			session.room = session.room.go(form.action)
+
+		web.seeother("/game")
 
 if __name__=="__main__":
 	app.run()
